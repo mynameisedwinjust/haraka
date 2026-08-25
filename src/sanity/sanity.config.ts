cb6dacfd57b7d9c2ef08be55c1f7a6483c97f6e9
mynameisedwinjust <e.muhire@alustudent.com>
2026-08-25 17:30:16 +0200

    Add built-in presentation tool and logo

--- src/sanity/sanity.config.ts
+++ src/sanity/sanity.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
+import { presentationTool } from 'sanity/presentation'
 import { schemaTypes } from './schemaTypes'
+import { StudioLogo } from './components/StudioLogo'
 
 export default defineConfig({
   name: 'default',
@@ -11,8 +13,17 @@ export default defineConfig({
 
   basePath: '/admin',
 
+  studio: {
+    components: {
+      logo: StudioLogo,
+    },
+  },
+
   plugins: [
     structureTool(),
+    presentationTool({
+      previewUrl: '/',
+    }),
   ],
 
   schema: {
